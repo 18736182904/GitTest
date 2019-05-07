2d8dbd6fa11ad6aeac53b101b2b52e77c1b9d29e
user <user@qq.com>
2019-05-07 14:22:12 +0800

    添加代码

--- src/cn/ten/TestGitHub.java
+++ src/cn/ten/TestGitHub.java
@@ -3,5 +3,6 @@ package cn.ten;
 public class TestGitHub {
         public static void main(String[] args) {
             System.out.println("上传代码");
+            System.out.println("我添加代码");
         }
 }
